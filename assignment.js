a070713e31e3e0365d122bff679774fbe5a747ab
wahidremon <wahidremon@gmail.com>
2022-11-14 20:52:34 +0600

    kilometerToMeter problem solve updated

---
+++ assignment.js
@@ -0,0 +1,10 @@
+// https://github.com/wahidremon/assignment
+
+// kilometerToMeter
+function kilometerToMeter(kilometerLength) {
+    var meterLength = kilometerLength * 1000;
+    return meterLength;
+}
+
+var result = kilometerToMeter(57);
+console.log(result);
