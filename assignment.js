7ec671a5bc96cb4102b10fd953d3f49d0b6d46e8
wahidremon <wahidremon@gmail.com>
2022-11-15 07:21:22 +0600

    budgetCalculator problem solve

--- assignment.js
+++ assignment.js
@@ -8,3 +8,32 @@ function kilometerToMeter(kilometerLength) {
 
 var result = kilometerToMeter(57);
 console.log(result);
+
+// budgetCalculator
+var watchPrice = 50;
+var phonePrice = 100;
+var laptopPrice = 500;
+
+function budgetCalculator(watchNmuber, phoneNumber, laptopNumber) {
+    
+    if(watchNmuber < 0) {
+        watchNmuber = 0;
+    }
+    watchNmuber = watchNmuber * watchPrice;
+
+    if(phoneNumber < 0) {
+        phoneNumber = 0;
+    }
+    phoneNumber = phoneNumber * phonePrice;
+
+    if(laptopNumber < 0) {
+        laptopNumber = 0;
+    }
+    laptopNumber = laptopNumber * laptopPrice;
+    
+    var totalnumber = watchNmuber + phoneNumber + laptopNumber;
+    return totalnumber; 
+}
+
+var result = budgetCalculator(0, 1, 2);
+console.log(result);
